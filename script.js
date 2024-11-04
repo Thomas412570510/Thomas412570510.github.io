9a4d7333e8f7f5ebd87db2ffe8b05c3e895f127c
Thomas412570510 <kobe20041124@gmail.com>
2024-11-04 10:47:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,28 +7,34 @@ $(document).ready(function() {
         attribution: '© OpenStreetMap'
     }).addTo(map);
 
-    const apiUrl = 'CWA-D9D189A8-D4BF-4C99-9885-B46EBB12D61E'; // 請替換為您的 API 金鑰
+    const apiUrl = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/T-A0031-001?Authorization=CWA-D9D189A8-D4BF-4C99-9885-B46EBB12D61E'; // 正確的 API URL
 
     $.getJSON(apiUrl, function(data) {
-        const typhoons = data.records.location;
+        if (data.records && data.records.location) {
+            const typhoons = data.records.location;
 
-        typhoons.forEach(typhoon => {
-            const name = typhoon.typhoonName; // 颱風名稱
-            const forecastPath = typhoon.forecastPath; // 預測路徑
-            const intensity = typhoon.intensity; // 預測強度
+            typhoons.forEach(typhoon => {
+                const name = typhoon.typhoonName; // 颱風名稱
+                const forecastPath = typhoon.forecastPath; // 預測路徑
+                const intensity = typhoon.intensity; // 預測強度
 
-            // 顯示颱風路徑
-            const pathCoords = forecastPath.map(point => [point.lat, point.lon]);
-            const polyline = L.polyline(pathCoords, { color: 'red', weight: 3 }).addTo(map);
+                // 顯示颱風路徑
+                const pathCoords = forecastPath.map(point => [point.lat, point.lon]);
+                const polyline = L.polyline(pathCoords, { color: 'red', weight: 3 }).addTo(map);
 
-            // 在地圖上標示颱風名稱和強度
-            L.marker(pathCoords[0]).addTo(map)
-                .bindPopup(`颱風名稱: ${name}<br>預測強度: ${intensity}級`)
-                .openPopup();
-        });
+                // 在地圖上標示颱風名稱和強度
+                L.marker(pathCoords[0]).addTo(map)
+                    .bindPopup(`颱風名稱: ${name}<br>預測強度: ${intensity}級`)
+                    .openPopup();
+            });
 
-        // 自動調整地圖顯示範圍
-        const bounds = L.latLngBounds(typhoons.map(typhoon => typhoon.forecastPath.map(point => [point.lat, point.lon])).flat());
-        map.fitBounds(bounds);
+            // 自動調整地圖顯示範圍
+            const bounds = L.latLngBounds(typhoons.map(typhoon => typhoon.forecastPath.map(point => [point.lat, point.lon])).flat());
+            map.fitBounds(bounds);
+        } else {
+            console.error("未能獲取颱風資料");
+        }
+    }).fail(function() {
+        console.error("無法連接到 API");
     });
 });
